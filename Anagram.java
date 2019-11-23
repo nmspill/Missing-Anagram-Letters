e02cc43411cde0c8bc12c5671bd95df6c02d686c
nmspill <31868753+nmspill@users.noreply.github.com>
2019-11-23 15:54:25 -0500

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -2,32 +2,31 @@
 public class Anagram {
 
 	public static void main(String[] args) {
-		anagram("melon", "lemon");	
-		anagram("tom", "brady");
-		anagram("kemba", "walker");
+		anagram("melon", "lemon"); //will print 0	
+		anagram("tom", "brady"); //will print 8
+		anagram("kemba", "walker"); //will print 5
 	}
 	
 	//given two strings, this will print out how many letters need to be added to either string
 	//so that they will be anagrams of each other
 	public static void anagram(String A, String B) { 
 		
-		if(A.equals(B)) {
-			System.out.println("0");
-			return;
+		if(A.equals(B)){
+		   System.out.println("0");
+	       return;
 		}
 		
-		String str1 = A+B;
-		String str = str1.toLowerCase();
+		String str1 = A+B; //concatenates the strings
+		String str = str1.toLowerCase(); //makes the string all lowercase
 	
-        char[] stringArr = new char[str.length()];
+        char[] stringArr = new char[str.length()]; //creates an array of characters
 
-		for (int i = 0; i < str.length(); i++) { 
+		for (int i = 0; i < str.length(); i++) { //fills the array with each character from the string
             stringArr[i] = str.charAt(i); 
- 
         }
 
 		int oddCount = 0;
-        for (int i = 0; i < stringArr.length; i++) { 
+        for (int i = 0; i < stringArr.length; i++) { //nested for loop counts if there is an odd number of a character in the array
             int count = 0; 
             for (int j = 0; j < stringArr.length; j++) { 
                 if (stringArr[i] == stringArr[j]) 
@@ -37,7 +36,7 @@ public class Anagram {
                 oddCount++; 	
             }
         } 
-        System.out.println(oddCount);    
+        System.out.println(oddCount);     
 	}
 
 }
